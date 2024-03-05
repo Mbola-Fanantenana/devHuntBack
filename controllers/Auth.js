642da8e9e9bbee1de6f6c015149969c4bb071da3
Mbola Ramanandraibe <mbola.ram26@gmail.com>
2024-03-05 15:49:39 +0300

    return user

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -53,7 +53,7 @@ const login = async (req, res) => {
 
         const token = generateJwtToken(user.idUser)
 
-        res.status(200).json({ token })
+        res.status(200).json({ user: user, token })
     } catch (error) {
         res.status(401).json({ msg: error.message })
     }
