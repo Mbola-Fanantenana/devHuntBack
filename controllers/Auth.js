5fd3a1452a0f1499291f31f4a0371e3d64ae6b2f
Andokely <andoratovo12@gmail.com>
2024-03-08 16:52:42 +0300

    API de face authentification

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const generateJwtToken = require("../jwt");
 
 const upload = require('../mutler');
+const { use } = require("../routes/Auth");
 
 const getAllUsers = async (req, res) => {
   try {
@@ -23,28 +24,28 @@ const signup = async (req, res) => {
       adresseUtilisateur,
       telUtilisateur,
       niveauUtilisateur,
-      matriculeUtilisateur, 
-      pseudoUtilisateur, 
-      emailUtilisateur, 
-      mdpUtilisateur 
+      matriculeUtilisateur,
+      pseudoUtilisateur,
+      emailUtilisateur,
+      mdpUtilisateur
     } = req.body;
 
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(mdpUtilisateur, saltRounds);
 
     const user = await prisma.utilisateur.create({
-        data: {
-            nomUtilisateur: nomUtilisateur,
-            prenomUtilisateur: prenomUtilisateur,
-            adresseUtilisateur: adresseUtilisateur,
-            telUtilisateur: telUtilisateur,
-            niveauUtilisateur: niveauUtilisateur,
-            matriculeUtilisateur: matriculeUtilisateur,
-            pseudoUtilisateur: pseudoUtilisateur,
-            emailUtilisateur: emailUtilisateur,
-            imgUtilisateur: req.file? req.file.path : null,
-            mdpUtilisateur: hashedPassword
-        }
+      data: {
+        nomUtilisateur: nomUtilisateur,
+        prenomUtilisateur: prenomUtilisateur,
+        adresseUtilisateur: adresseUtilisateur,
+        telUtilisateur: telUtilisateur,
+        niveauUtilisateur: niveauUtilisateur,
+        matriculeUtilisateur: matriculeUtilisateur,
+        pseudoUtilisateur: pseudoUtilisateur,
+        emailUtilisateur: emailUtilisateur,
+        imgUtilisateur: req.file ? req.file.path : null,
+        mdpUtilisateur: hashedPassword
+      }
     });
 
     res.status(201).json(user)
@@ -55,33 +56,62 @@ const signup = async (req, res) => {
 };
 
 const login = async (req, res) => {
-    try {
-        const { pseudoUtilisateur, mdpUtilisateur } = req.body
+  try {
+    const { pseudoUtilisateur, mdpUtilisateur } = req.body
 
-        if (pseudoUtilisateur == null) {
-            throw new error('pseudo undefined')
-        }
-
-        const user = await prisma.utilisateur.findUnique({
-            where: {
-                pseudoUtilisateur
-            }
-        })
-
-        if (!user || !bcrypt.compareSync(mdpUtilisateur, user.mdpUtilisateur)) {
-            return res.status(401).json({ error: "Informations d'identification incorrect" })
-        }
-
-        const token = generateJwtToken(user.idUser)
-
-        res.status(200).json({ user: user, token })
-    } catch (error) {
-        res.status(401).json({ msg: error.message })
+    if (pseudoUtilisateur == null) {
+      throw new error('pseudo undefined')
     }
+
+    const user = await prisma.utilisateur.findUnique({
+      where: {
+        pseudoUtilisateur
+      }
+    })
+
+    if (!user || !bcrypt.compareSync(mdpUtilisateur, user.mdpUtilisateur)) {
+      return res.status(401).json({ error: "Informations d'identification incorrect" })
+    }
+
+    const token = generateJwtToken(user.idUser)
+
+    res.status(200).json({ user: user, token })
+  } catch (error) {
+    res.status(401).json({ msg: error.message })
+  }
+}
+
+const faceAuth = async (req, res) => {
+  const { pseudoUtilisateur } = req.body
+
+  try {
+
+    if (pseudoUtilisateur == null) {
+      throw new error('pseudo undefined')
+    }
+
+    const user = await prisma.utilisateur.findFirst({
+      where: {
+        pseudoUtilisateur: pseudoUtilisateur
+      }
+    });
+
+    const token = generateJwtToken(user.idUser);
+    const userWithToken = {};
+
+    userWithToken["token"] = token;
+    userWithToken["user"] = user;
+    
+    res.status(200).json(userWithToken);
+
+  } catch (error) {
+    res.status(401).json({ error: error.message })
+  }
 }
 
 module.exports = {
   getAllUsers,
   signup,
-  login
+  login,
+  faceAuth
 };
